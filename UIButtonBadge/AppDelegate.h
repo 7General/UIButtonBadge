b01fe073667c73f7b6b3e508ec4e2bee7dab17c1
王会洲 <wanghuizhou21@163.com>
2016-07-11 14:02:52 +0800

    创建工程基本文件

---
+++ UIButtonBadge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIButtonBadge
+//
+//  Created by 王会洲 on 16/7/11.
+//  Copyright © 2016年 王会洲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIButtonBadge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIButtonBadge
+//
+//  Created by 王会洲 on 16/7/11.
+//  Copyright © 2016年 王会洲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
